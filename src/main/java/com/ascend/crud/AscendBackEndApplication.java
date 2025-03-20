313e90f3a4ca6555905f17ba6da93bab2d92b010
Mr Back Ally Cat <krittamateza@gmail.com>
2025-03-20 22:28:53 +0700

    First Commit

---
+++ src/main/java/com/ascend/crud/AscendBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ascend.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscendBackEndApplication.class, args);
+	}
+
+}
